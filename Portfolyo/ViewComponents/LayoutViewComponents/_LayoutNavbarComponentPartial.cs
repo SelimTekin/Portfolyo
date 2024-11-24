32231af27e8b53375f1015dda5b4ae2c9e97a9ff
SelimTekin <tekinselim.57.57@gmail.com>
2024-11-24 20:26:19 +0300

    done

--- Portfolyo/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ Portfolyo/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolyo.DAL.Context;
 
 namespace Portfolyo.ViewComponents.LayoutViewComponents
 {
 	public class _LayoutNavbarComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke() { return View(); }
+		PortfolioContext context = new PortfolioContext();
+		public IViewComponentResult Invoke() {
+			ViewBag.toDoListCount = context.ToDoLists.Where(x=>x.Status == false).Count();
+			var values = context.ToDoLists.Where(x=>x.Status == false).ToList();
+			return View(values);
+		}
 	}
 }
